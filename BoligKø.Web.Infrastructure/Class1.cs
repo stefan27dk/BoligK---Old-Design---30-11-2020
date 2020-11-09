a2ad479700e27e1540e1b702c08768f36d6eef38
Nichlas Christensen <nichlaschristensen@live.dk>
2020-11-09 12:39:17 +0100

    Arkitektur

---
+++ BoligKø.Web.Infrastructure/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace BoligKø.Web.Infrastructure
+{
+    public class Class1
+    {
+    }
+}
---
+++ Boligkø.Web.ApplicationService/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Boligkø.Web.ApplicationService
+{
+    public class Class1
+    {
+    }
+}
