219e62917fed628af17396cef144d1871ac01136
Martin Schmidt Sørensen <mart95s9@edu.ucl.dk>
2020-11-23 20:57:18 +0100

    Blah blah blah API Endpoints
    VIGTIG: Port til API ændret til 5002 for HTTPS og 5003 for HTTP

---
+++ BoligKø.Api/Mapping/Mapping.cs
@@ -0,0 +1,47 @@
+﻿using BoligKø.ApplicationService.Dto;
+using BoligKø.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BoligKø.Api
+{
+    public class Mapping
+    {
+        public static AnsøgerDto MapAnsøger(Ansøger ansøger)
+        {
+            AnsøgerDto dto = new AnsøgerDto()
+            {
+                Efternavn = ansøger.Efternavn,
+                Fornavn = ansøger.Fornavn,
+                Email = ansøger.Email,
+                UserId = ansøger.UserId,
+                Id = ansøger.Id,
+                Ansøgninger = new List<AnsøgningDto>()
+            };
+
+            AddAnsøgnings(ansøger.Ansøgninger, dto);
+
+            return dto;
+        }
+
+        private static void AddAnsøgnings(IEnumerable<Ansøgning> ansøgnings, AnsøgerDto dto)
+        {
+            foreach(Ansøgning a in ansøgnings)
+            {
+                AnsøgningDto ansøgningDto = MapAnsøgning(a);
+                dto.Ansøgninger.Add(ansøgningDto);
+            }
+        }
+
+        public static AnsøgningDto MapAnsøgning(Ansøgning a)
+        {
+            AnsøgningDto dto = new AnsøgningDto();
+            //TODO: Map ansøgningsDto når der er Set metoder på 
+
+            return dto;
+            
+        }
+    }
+}
