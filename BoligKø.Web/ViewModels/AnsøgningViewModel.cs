df214c4d1c03b8886edd442909e243bbe0e19753
Stefan Biserov Popov <stef9633@edu.ucl.dk>
2020-11-26 14:03:06 +0100

    Validation Fixes - AnsøgningViewModel

--- BoligKø.Web/ViewModels/AnsøgningViewModel.cs
+++ BoligKø.Web/ViewModels/AnsøgningViewModel.cs
@@ -11,75 +11,107 @@ namespace BoligKø.Web.ViewModels
     {
         //public AnsøgerViewModel Ansøger { get; set; }
 
+
+        // ==== [UserId - LogIn] =============================================  
         public string UserId { get; set; }
 
-        [MaxLength(500, ErrorMessage = "Komentar kan ikke overskride 200 bogstaver")]
-        [Display(Name = "Øvrige Kommentar")]   
-        [Required]
-        public string ØvrigeKommentar { get; set; }
 
 
-        [Range(0,99999, ErrorMessage = "Fra m2 - Maks 5 tegn er tilladt")]
-        [Display(Name = "Fra m2")]
-        [Required]
-        public double FraKvm { get; set; }
-
-
-        [Range(0,99999, ErrorMessage = "Til m2 - Maks 5 tegn er tilladt")]
-        [CompareNumbers("FraKvm", ErrorMessage = "Til m2 skal være større end Fra m2")]
-        [Display(Name = "Til m2")]    
-        [Required]
-        public double TilKvm { get; set; }
-
-
-        [Range(0,99999, ErrorMessage = "Fra Pris - Maks 5 tegn er tilladt")]
-        [Display(Name = "Fra Pris")]     
-        [Required]
-        public double FraPris { get; set; }
-
-
-        [Range(0,99999, ErrorMessage = "Til Pris - Maks 5 tegn er tilladt")]
-        [CompareNumbers("FraPris", ErrorMessage = "Til Pris skal være større end Fra Pris")] // On Submit Validation
-        [Display(Name = "Til Pris")]   
-        [Required]
-        public double TilPris { get; set; }
-
-
-        [Range(0,99, ErrorMessage = "Fra Antal Værelser - Maks 2 tal er tilladt")]
-        [Display(Name = "Fra Antal Værelser")]
-        [Required]
-        public int FraAntalVærelser { get; set; }
-
-
-        [Range(0,99, ErrorMessage = "Til Antal Værelser - Maks 2 tal er tilladt")]  
-        [CompareNumbers("FraAntalVærelser", ErrorMessage = "Til Antal Værelser skal være større end Fra Antal Værelser")]  
-        [Display(Name = "Til Antal Værelser")]     
-        [Required]
-        public int TilAntalVærelser { get; set; }
-                
-
-
-        [Display(Name = "Små Dyr")]    
-        [Required]
-        public bool SmåDyr { get; set; }
-
-
-
-        [Display(Name ="Stor Dyr")]   
-        [Required]
-        public bool StorDyr { get; set; }
-
-
-        [Range(0,9999, ErrorMessage = "Post Nr - Maks 4 tal er tilladt")]  
-        [Display(Name = "Post Nr")]    
-        [Required]
+        // ==== [Post Nr] ===================================================
+        [Range(0555, 9999, ErrorMessage = "Post Nr - Min / Max - 4 tal")]
+        [Display(Name = "Post Nr")]
+        [Required(ErrorMessage = "Post Nr - kan ikke være tom")]
         public int PostNr { get; set; }
 
 
 
+
+
+
+        // ==== [Dyr] ======================================================    
+        [Display(Name = "Små Dyr")]
+        public bool SmåDyr { get; set; }
+
+
+
+        [Display(Name = "Stor Dyr")]
+        public bool StorDyr { get; set; }
+
+
+
+
+
+
+        // ==== [Antal Værelser] ===================================================    
+        [Range(1, 20, ErrorMessage = "Fra Antal Værelser - Min 1 og Max 20")]
+        [Display(Name = "Fra Antal Værelser")]
+        [Required(ErrorMessage = "Fra Antal Værelser - kan ikke være tom")]
+        public int FraAntalVærelser { get; set; }
+
+
+        [Range(1, 20, ErrorMessage = "Til Antal Værelser - Min 1 og Max 20")]
+        [CompareNumbers("FraAntalVærelser", ErrorMessage = "Til Antal Værelser skal være større end Fra Antal Værelser")]
+        [Display(Name = "Til Antal Værelser")]
+        [Required(ErrorMessage = "Til Antal Værelser - kan ikke være tom")]
+        public int TilAntalVærelser { get; set; }
+
+
+
+
+
+
+        // ==== [Pris] ===================================================      
+        [Range(500,20000, ErrorMessage = "Fra Pris - Min 500kr og Max 20.000kr")]
+        [Display(Name = "Fra Pris")]
+        [Required(ErrorMessage = "Fra Pris - kan ikke være tom")]   
+        public double FraPris { get; set; }
+
+
+        [Range(500,20000, ErrorMessage = "Til Pris - Min 500kr og Max 20.000kr")]
+        [CompareNumbers("FraPris", ErrorMessage = "Til Pris skal være større end Fra Pris")] // On Submit Validation
+        [Display(Name = "Til Pris")]
+        [Required(ErrorMessage = "Til Pris - kan ikke være tom")]   
+        public double TilPris { get; set; }
+
+
+
+
+
+
+
+        // ==== [Kvadratmeter] ===================================================    
+        [Range(5, 800, ErrorMessage = "Fra m2 - Min 5 m2 og Max 800 m2")]
+        [Display(Name = "Fra m2")]
+        [Required(ErrorMessage = "Fra m2 kan ikke være tom")]
+        public double FraKvm { get; set; }
+
+
+        [Range(5, 800, ErrorMessage = "Til m2 - Min 5 m2 og Max 800 m2")]
+        [CompareNumbers("FraKvm", ErrorMessage = "Til m2 - skal være større end Fra m2")]
+        [Display(Name = "Til m2")]
+        [Required(ErrorMessage = "Til m2 - kan ikke være tom")]
+        public double TilKvm { get; set; }
+
+
+
+
+
+
+
+        // ==== [Lejemåls Type] ===================================================   
         [Display(Name ="Lejemåls Type")]
-        [Required]
+        [Required(ErrorMessage = "Lejemåls Type - kan ikke være tom")]    
         public LejemålTypeKriterie LejemålsType { get; set; }
+
+
+
+
+
+        // ==== [Øvrige Kommentar] ===================================================  
+        [MaxLength(500, ErrorMessage = "Øvrige Kommentar - kan ikke overskride 500 tegn")]
+        [Display(Name = "Øvrige Kommentar")]
+        [Required(ErrorMessage = "Øvrige Kommentar - kan ikke være tom")]
+        public string ØvrigeKommentar { get; set; }
         //public IEnumerable<IKriterie> Kriterier { get; private set; }
 
 
@@ -89,7 +121,10 @@ namespace BoligKø.Web.ViewModels
 
 
 
-        // Custome Anotation - || Class ||
+
+
+
+        // ==========================# Custome Anotation Validation - || Class || #=========================================================
         public class CompareNumbers : ValidationAttribute
         {
             private readonly string _comparisonProperty;
