48c56c9a1757870ec2be377049f6e113b7a23bcf
karl mogensen <karlmogensen@hotmail.com>
2020-11-28 20:26:21 +0100

    creating ansøgning now adds all kriterier to db

--- BoligKø.Infrastructure/Commands/AnsøgningCommand.cs
+++ BoligKø.Infrastructure/Commands/AnsøgningCommand.cs
@@ -1,7 +1,10 @@
 ﻿using BoligKø.ApplicationService;
 using BoligKø.Domain.Model;
 using BoligKø.Infrastructure.context;
+using BoligKø.Infrastructure.Exceptions;
 using BoligKø.Infrastructure.patterns;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace BoligKø.Infrastructure.Commands
 {
@@ -10,5 +13,14 @@ namespace BoligKø.Infrastructure.Commands
         public AnsøgningCommand(BoligKøContext context) : base(context)
         {
         }
+
+        protected override void HandleEntityBeforeDataChangeOperation(Ansøgning entity)
+        { 
+            var trackedAnsøger = context.Ansøgere.FirstOrDefault(a => a.Id == entity.Ansøger.Id);
+            if (trackedAnsøger == null)
+                throw new NoValidNavigationEntityFoundException("Ansøgningens ansøger findes ikke i databasen"); 
+            entity.SetAnsøger(trackedAnsøger);
+        }
+
     }
 }
